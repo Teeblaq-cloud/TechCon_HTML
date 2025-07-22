075b00bc2c10ac19cd9b6f80e4576af64dff9314
Tee_blaq <olaifatimileyin2017@gmail.com>
2025-07-22 16:50:40 +0100

    calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+operator = input('Enter the operator (+, -, *, /): ')
+match operator:
+    case '+':
+        print(f'The result is: {num1 + num2}')
+    case '-':
+        print(f'The result is: {num1 - num2}')
+    case '*':
+        print(f'The result is: {num1 * num2}')
+    case '/':
+        print(f'The result is: {num1 / num2}' if num2 != 0 else 'Error: Division by zero')
+    case _:
+        print('Invalid operator')
